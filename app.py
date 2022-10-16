f0b0edca4b0ca3d98b54155294b4ac7567d2056d
Travis Alstrand <travisalstrand@gmail.com>
2022-10-16 13:09:47 -0700

    converted guardians into list

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import copy
 from constants import PLAYERS
+from constants import TEAMS
 
 
+# create a new list to not affect original
 def create_new_list(list):
   new_list = copy.deepcopy(list)
   return new_list
 
 
-def convert_int(string):
-  substring = ' inches'
-  new_string = string.replace(substring, '')
-  new_int = int(new_string)
-  return new_int
-
-
+# convert PLAYER experience to a BOOL
 def convert_bool(string):
   bool = None
   if string == 'YES':
@@ -23,15 +19,37 @@ def convert_bool(string):
   return bool
 
 
+# convert PLAYER height to an INT
+def convert_int(string):
+  substring = ' inches'
+  new_string = string.replace(substring, '')
+  new_int = int(new_string)
+  return new_int
+
+def convert_to_list(string):
+  substring = ' and '
+  if substring in string:
+    new_list = string.split(substring)
+  else:
+    new_list = [string]
+  return new_list
+
+# replace PLAYER experience / height with new values
 def clean_data(list):
   for dict in list:
     new_exp = convert_bool(dict['experience'])
     dict['experience'] = new_exp
     new_height = convert_int(dict['height'])
     dict['height'] = new_height
+    new_guardians = convert_to_list(dict['guardians'])
+    dict['guardians'] = new_guardians
 
 
 if __name__ == "__main__":
   new_players = create_new_list(PLAYERS)
+  new_teams = create_new_list(TEAMS)
   clean_data(new_players)
+
+print(new_players)
+  # print(len(PLAYERS) / len(TEAMS))
 
