95f8c3b4c05b07c08a2e10012534a059db068c53
Travis Alstrand <travisalstrand@gmail.com>
2022-10-16 12:24:24 -0700

    converted height to an int

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ def create_new_list(list):
   return new_list
 
 
-# def convert_int(string):
-#   new_int = int(string)
-#   return new_int
+def convert_int(string):
+  substring = ' inches'
+  new_string = string.replace(substring, '')
+  new_int = int(new_string)
+  return new_int
 
 
 def convert_bool(string):
@@ -25,8 +27,11 @@ def clean_data(list):
   for dict in list:
     new_exp = convert_bool(dict['experience'])
     dict['experience'] = new_exp
+    new_height = convert_int(dict['height'])
+    dict['height'] = new_height
 
 
 if __name__ == "__main__":
   new_players = create_new_list(PLAYERS)
   clean_data(new_players)
+
