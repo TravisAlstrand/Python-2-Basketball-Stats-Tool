f1501e774723f5efb98660dab0aae1761a24e3b9
Travis Alstrand <travisalstrand@gmail.com>
2022-10-16 16:17:28 -0700

    not finished dividing teams evenly. need more brains

--- app.py
+++ app.py
@@ -26,6 +26,40 @@ def convert_int(string):
   new_int = int(new_string)
   return new_int
 
+
+def balance_teams(teams, players):
+  experienced_players = []
+  not_experienced_players = []
+  div_length = (len(players) / 2) / len(teams)
+  balanced_team = {}
+
+  for team in teams:
+    balanced_team.update({
+      team : []
+    })
+
+  for player in players:
+    if player['experience'] == True:
+      experienced_players.append(player)
+    else:
+      not_experienced_players.append(player)
+
+  split_exp = [] 
+  for i in range(0, len(experienced_players), int(div_length)):
+    split_exp.append(experienced_players[i])
+
+  split_no_exp = []
+  for i in range(0, len(not_experienced_players), int(div_length)):
+    split_no_exp.append(not_experienced_players[i])
+  
+  print(split_exp)
+  print(split_no_exp)
+
+    
+  # print(balanced_team)
+
+
+# convert PLAYER guardians to a LIST
 def convert_to_list(string):
   substring = ' and '
   if substring in string:
@@ -34,22 +68,18 @@ def convert_to_list(string):
     new_list = [string]
   return new_list
 
-# replace PLAYER experience / height with new values
+
+# replace PLAYER experience / height / guardians with new values
 def clean_data(list):
   for dict in list:
-    new_exp = convert_bool(dict['experience'])
-    dict['experience'] = new_exp
-    new_height = convert_int(dict['height'])
-    dict['height'] = new_height
-    new_guardians = convert_to_list(dict['guardians'])
-    dict['guardians'] = new_guardians
+    dict['experience'] = convert_bool(dict['experience'])
+    dict['height'] = convert_int(dict['height'])
+    dict['guardians'] = convert_to_list(dict['guardians'])
 
 
+# Dunder Main statement
 if __name__ == "__main__":
   new_players = create_new_list(PLAYERS)
   new_teams = create_new_list(TEAMS)
   clean_data(new_players)
-
-print(new_players)
-  # print(len(PLAYERS) / len(TEAMS))
-
+  balance_teams(new_teams, new_players)
